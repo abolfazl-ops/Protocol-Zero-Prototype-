19aa31d403ee933bc0bb57c05cd24528f009375e
Abolfazl Hajinasiri <abolfazlproxer@gmail.com>
2026-07-29 19:40:21 +0330

    Add files via upload

---
+++ PlayerLook.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerLook : MonoBehaviour
+{
+    public float mouseSensitivity = 50f;
+    public Transform cam;
+
+    private float xRotation = 0f;
+    private Vector2 lookInput;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void OnLook(InputValue value)
+    {
+        lookInput = value.Get<Vector2>();
+    }
+
+    void Update()
+    {
+        HandleMouseLook();
+    }
+
+    void HandleMouseLook()
+    {
+        float mouseX = lookInput.x * mouseSensitivity * Time.deltaTime;
+        float mouseY = lookInput.y * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        cam.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        transform.Rotate(Vector3.up * mouseX);
+    }
+}
