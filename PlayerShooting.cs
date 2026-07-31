abcf436650d42785f0bf43ec45e05a3e364bc79c
Abolfazl Hajinasiri <abolfazlproxer@gmail.com>
2026-07-31 23:35:12 +0330

    PlayerShooting.cs

--- PlayerShooting.cs
+++ PlayerShooting.cs
@@ -1,117 +1,47 @@
-using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class Gun : MonoBehaviour
+public class PlayerShooting : MonoBehaviour
 {
-    [Header("Gun Settings")]
-    public float reloadTime = 1f;
-    public float fireRate = 0.15f;
-    public int maxSize = 20;
-    public GameObject bullet;
-    public Transform bulletSpawnPoint;
-    public GameObject weaponFlash;
-    public GameObject droppedWeapon;
+    public Gun gun;
+    public Transform gunHolder;
+    private bool isHoldingShoot;
 
-    [Header("Recoil Settings")]
-    public float recoilDistance = 0.1f;
-    public float recoilSpeed = 15f;
-    public Vector3 reloadRotationOffset = new Vector3(60, 50, 50);
-
-    private int currentAmmo;
-    private bool isReloading = false;
-    private float nextTimeToFire = 0f;
-    private Quaternion initialRotation;
-    private Vector3 initialPosition;
-
-    void Start()
+// Triggered by 'Shoot' action in Input System
+    public void OnShoot()
     {
-        currentAmmo = maxSize;
-        initialRotation = transform.localRotation;
-        initialPosition = transform.localPosition;
+        isHoldingShoot = true;
     }
 
-    public void Shoot()
+// Triggered by 'Shoot Release' action
+    public void OnShootRelease()
     {
-        if (isReloading || Time.time < nextTimeToFire) return;
-
-        if (currentAmmo <= 0)
-        {
-            StartCoroutine(Reload());
-            return;
-        }
-
-        nextTimeToFire = Time.time + fireRate;
-        currentAmmo--;
-
-        Instantiate(bullet, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-        Instantiate(weaponFlash, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-
-        StopCoroutine(nameof(Recoil));
-        StartCoroutine(nameof(Recoil));
+        isHoldingShoot = false;
     }
 
-    public void TryReload()
+// Triggered by 'Reload' action (Pressing R)
+    public void OnReload()
     {
-        if (!isReloading && currentAmmo < maxSize)
+        if (gun != null)
         {
-            StartCoroutine(Reload());
+            gun.TryReload();
         }
     }
 
-    private IEnumerator Reload()
+    void Update()
     {
-        isReloading = true;
-        Quaternion targetRotation = Quaternion.Euler(initialRotation.eulerAngles + reloadRotationOffset);
-        float halfReload = reloadTime / 2;
-        float t = 0;
-
-        // Rotate to reload position
-        while (t < halfReload)
+        // Continuously fire if holding the mouse button
+        if (isHoldingShoot && gun != null)
         {
-            t += Time.deltaTime;
-            transform.localRotation = Quaternion.Lerp(initialRotation, targetRotation, t / halfReload);
-            yield return null;
+            gun.Shoot();
         }
-
-        // Rotate back
-        t = 0;
-        while (t < halfReload)
-        {
-            t += Time.deltaTime;
-            transform.localRotation = Quaternion.Lerp(targetRotation, initialRotation, t / halfReload);
-            yield return null;
-        }
-
-        currentAmmo = maxSize;
-        isReloading = false;
     }
 
-    private IEnumerator Recoil()
+    public void OnDrop()
     {
-        Vector3 recoilTarget = initialPosition + new Vector3(0, 0, recoilDistance);
-        float t = 0;
-
-        while (t < 1f)
+        if (gun != null)
         {
-            t += Time.deltaTime * recoilSpeed;
-            transform.localPosition = Vector3.Lerp(initialPosition, recoilTarget, t);
-            yield return null;
+            gun.Drop();
         }
-
-        t = 0;
-        while (t < 1f)
-        {
-            t += Time.deltaTime * recoilSpeed;
-            transform.localPosition = Vector3.Lerp(recoilTarget, initialPosition, t);
-            yield return null;
-        }
-
-        transform.localPosition = initialPosition;
-    }
-
-    public void Drop()
-    {
-        Instantiate(droppedWeapon, transform.position, transform.rotation);
-        Destroy(gameObject);
     }
 }
